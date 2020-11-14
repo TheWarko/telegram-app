10fb4d860cd7784fd3dc346d56fdd2824e709f19
Marco Savastio <marco.savastio@qmi.it>
2020-11-14 13:31:11 +0100

    now saving history chat

--- src/assets/js/Io.js
+++ src/assets/js/Io.js
@@ -2,17 +2,28 @@ import io from 'socket.io-client';
 
 const Io = () => {
 
+    //init
     var socket = io('http://localhost:3000');
     socket.on('connect', onConnect);
-    function onConnect() {
-        console.log('connect ' + socket.id);
-        socket.on('disconnect', () => console.log('disconnect ' + socket.id));
-    }
-
 
     const msgButton = document.getElementById('send')
     const msgInput = document.getElementById('message')
     const msgArea = document.getElementById('messages')
+
+    function onConnect() {
+        console.log('connect ' + socket.id);
+        socket.on('disconnect', () => console.log('disconnect ' + socket.id));        
+    }
+
+    //on load
+    socket.on('load', function(msgs) {
+        console.log(msgs)
+        msgs.forEach((msg) =>{
+            let node = document.createElement('li')
+            node.innerHTML = msg
+            msgArea.appendChild(node)
+        })
+    });
 
     //send message
     msgButton.addEventListener('click',(ev) => {
--- src/server.js
+++ src/server.js
@@ -9,11 +9,15 @@ const port = process.env.PORT || 3000;
 server.listen(port);
 io.sockets.on('connect', onConnect);
 
+let msgs = []
+
 function onConnect(socket){
 
+    socket.emit('load',msgs)
     socket.on('message', function(msg) {
       socket.emit('messages',msg)
       socket.broadcast.emit('messages',msg)
+      msgs.push(msg)
     });
 
 }
